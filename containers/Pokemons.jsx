d04ed50bc64ddf1f2ccdf3f80660a13594ec2d76
Yann Mougenel <mougeny@bde.enseeiht.fr>
2017-01-24 21:44:33 +0100

    Forgot some files

--- containers/Pokemons.jsx
+++ containers/Pokemons.jsx
@@ -5,6 +5,17 @@
 import React from 'react';
 
 class Pokemons extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemons: {
+        [
+          name:"Bulbizaree",
+          thumbnail:"bulbizarre_by_rubidi-d6fjhl1"
+        ]
+      }
+    }
+  }
   render() {
     return (
       <div>There are no pokemon MF</div>
